f13ff4eb9046e3fceeca8d7fe3188a745cab800f
Vamilton <arteiano@yandex.ru>
2022-05-22 01:46:45 +0300

    Всё оказалось гораздо проще

--- Домашки/Stackoverflow.py
+++ Домашки/Stackoverflow.py
@@ -1,11 +1,9 @@
 import requests
 import time
-from datetime import date
 
 def q_about_what(tag):
-    d = date.today()
     url = "https://api.stackexchange.com/2.3/questions?fromdate="
-    need_url = f'{url}{(int(time.mktime(d.timetuple()))-172800+10800)}&todate={int(time.mktime(d.timetuple()))+10800}&order=desc&sort=creation&tagged={tag}&site=stackoverflow'
+    need_url = f'{url}{(int(time.time())-172800)}&todate={int(time.time())}&order=desc&sort=creation&tagged={tag}&site=stackoverflow'
     resp = requests.get(need_url)
     some_list = resp.json()['items']
     for i in some_list:
