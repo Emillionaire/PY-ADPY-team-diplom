de9d72c9b1413468e6a85ad70b77f5a4459915b0
Vamilton <arteiano@yandex.ru>
2022-05-21 20:07:36 +0300

    Всё работает, чо

---
+++ Домашки/Superheroes.py
@@ -0,0 +1,21 @@
+import requests
+
+url = "https://superheroapi.com/api/2619421814940190/search/"
+
+heroes = {}
+
+def get_intelligence(names):
+    for name in names:
+        name_url = url+name
+        resp = requests.get(name_url).json()
+        heroes[resp['results'][0]['name']] = resp['results'][0]['powerstats']['intelligence']
+    sorted_heroes = sorted(heroes.items(), key=lambda x: x[1])
+    return(sorted_heroes)
+
+def smartest_hero(names):
+    sorted_heroes = get_intelligence(names)
+    print(f'Самый умный герой - {max(sorted_heroes, key=lambda x: x[0])[0]}')
+
+
+if __name__ == '__main__':
+    smartest_hero(['Hulk', 'Captain_America', 'Thanos'])
