54fcba306e564bdd8a1af0da2b3e4cdc3963ca9b
Vamilton <103585697+Vamilton@users.noreply.github.com>
2022-08-26 18:59:43 +0300

    Add files via upload

--- parser.py
+++ parser.py
@@ -13,6 +13,11 @@ import re
 def write_msg(user_id, message):
     authorize.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random.randint(0, 2048)})
 
+def sql_add():
+    sql.add_person(p_info['response'][0]['id'],
+                   p_info['response'][0]['first_name'] + ' ' + p_info['response'][0]['last_name'],
+                   p_info['response'][0]['city']['id'], b_date, p_info['response'][0]['sex'])
+
 
 path = 'settings.ini'
 config = configparser.ConfigParser()
@@ -33,10 +38,12 @@ for event in longpoll.listen():
             print(b_date)
             if not b_date.isdigit():
                 write_msg(event.user_id, 'Укажите год рождения')
+            else:
+                sql_add()
         if event.to_me and event.text.isdigit():
             b_date = event.text
             print(b_date)
-            print(p_info['response'])
-        sql.add_person(p_info['response'][0]['id'], p_info['response'][0]['first_name']+' '+p_info['response'][0]['last_name'], p_info['response'][0]['city']['id'], b_date, p_info['response'][0]['sex'])
+            sql_add()
+
 
 
