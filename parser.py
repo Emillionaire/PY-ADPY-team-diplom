cbcfc0d5924b2d7caa9cc5c9613ecb248b2d57b4
Vamilton <103585697+Vamilton@users.noreply.github.com>
2022-08-26 17:59:36 +0300

    Add files via upload

--- parser.py
+++ parser.py
@@ -8,6 +8,7 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from Vk_get import VkUsers
 from sql import Sql_table
+import re
 
 def write_msg(user_id, message):
     authorize.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random.randint(0, 2048)})
@@ -26,11 +27,16 @@ sql = Sql_table()
 longpoll = VkLongPoll(authorize)
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
-        if event.to_me:
+        if event.to_me and not event.text.isdigit():
             p_info = vku.get_person_info(event.user_id)
+            b_date = p_info['response'][0]['bdate'][-4:]
+            print(b_date)
+            if not b_date.isdigit():
+                write_msg(event.user_id, 'Укажите год рождения')
+        if event.to_me and event.text.isdigit():
+            b_date = event.text
+            print(b_date)
             print(p_info['response'])
-            # sql.add_person(p_info['response'])
-            sql.add_person(p_info['response'][0]['id'], p_info['response'][0]['first_name']+' '+p_info['response'][0]['last_name'], p_info['response'][0]['city']['id'], p_info['response'][0]['bdate'][-4:])
-            # write_msg(event.user_id, "Хай")
-            # print(p_info['response'])
+            sql.add_person(p_info['response'][0]['id'], p_info['response'][0]['first_name']+' '+p_info['response'][0]['last_name'], p_info['response'][0]['city']['id'], b_date, p_info['response'][0]['sex'])
+
 
