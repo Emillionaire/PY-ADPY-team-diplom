e624870dc6d2cd2bb06bf17ebc0b40bef63bd410
Vamilton <103585697+Vamilton@users.noreply.github.com>
2022-08-27 19:27:06 +0300

    Add files via upload

--- parser.py
+++ parser.py
@@ -2,7 +2,9 @@ import configparser
 import requests
 from datetime import datetime
 import sys
+import json
 from pprint import pprint
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 import random
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
@@ -25,25 +27,39 @@ config.read(path)
 tok = config.get("Tokens", "bot_token")
 
 authorize = vk_api.VkApi(token=tok)
+vk = authorize.get_api()
 
 vku = VkUsers()
 sql = Sql_table()
+settings = dict(one_time=False, inline=True)
+keyboard = VkKeyboard(**settings)
+keyboard.add_button('Привет', color=VkKeyboardColor.NEGATIVE)
 
-longpoll = VkLongPoll(authorize)
+longpoll = VkLongPoll(authorize) #Слушаем чат
 for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        p_info = vku.get_person_info(event.user_id)
-        if event.to_me and event.text.lower() == 'привет':
-            b_date = p_info['response'][0]['bdate'][-4:]
-            print(b_date)
-            if not b_date.isdigit():
-                write_msg(event.user_id, 'Укажите год рождения')
-            else:
-                sql_add()
-        if event.to_me and event.text.isdigit():
-            b_date = event.text
-            print(b_date)
-            sql_add()
+    if event.type == VkEventType.MESSAGE_NEW: #Вот нам написали
+        if not sql.we_know_him(event.user_id): #Если его id ет в таблице person, то:
+            p_info = vku.get_person_info(event.user_id)  # дёргаем о пользователе информацию
+            if event.to_me and event.text.lower() == 'начать': #"начать" появляется по кнопке
+                b_date = p_info['response'][0]['bdate'][-4:] #проверяем, указан ли у него год рождения
+                if not b_date.isdigit():  #Если года рождения нет
+                    write_msg(event.user_id, 'Укажите год рождения') #спрашиваем его
+                else:
+                    sql_add() #если всё ок, сразу добавляем в БД
+            if event.to_me and event.text.isdigit(): #это он пишет нам год
+                b_date = event.text
+                sql_add() #и мы добавляем человека в БД
+        elif event.to_me:
+            write_msg(event.user_id, 'Lhfnenb') #на это пока не обращай внимание, это я кнопки пытаюсь сделать
+            vk.messages.send(
+                keyboard=keyboard.get_keyboard(),
+                random_id=random.randint(0, 2048),
+                key=('d888bf02174ccfde5be21361b1fbe65490434c8b'),
+                server=("https://lp.vk.com/wh72316872"),
+                ts=('1'),
+                message='Держи',
+                user_id=event.user_id
+            )
 
 
 
